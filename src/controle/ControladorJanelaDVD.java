9b995a10195a8dd3ba306f6b1a083afb3b44b995
Lara Fábian <87339100+Lara-Fabian-Almeida@users.noreply.github.com>
2022-06-30 23:23:03 -0300

    Update ControladorJanelaDVD.java

--- src/controle/ControladorJanelaDVD.java
+++ src/controle/ControladorJanelaDVD.java
@@ -1,11 +1,5 @@
 /*
- * Universidade Estadual do Sudoeste da Bahia - UESB
- * Curso: Sistemas de Informação     Semestre: 3º       Período Letivo: 2020.1
- * Disciplina: LP II      Professor: Murilo Santana
- * Aluno(a): Lara Fábian Almeida Santos
- * Data: 31/05/2021
- *
- * Criar interface gráfica de um protetor de tela de DVD, usando Thread e sleep.
+ * Criar interface grÃ¡fica de um protetor de tela de DVD, usando Thread e sleep.
  */
 
 package controle;
@@ -20,23 +14,23 @@ public class ControladorJanelaDVD {
 	//Construtor do ControladorJanelaDVD.
 	public ControladorJanelaDVD() {
 		this.janela = new JanelaDVD();
-		new MovimentoAutomatico().start();  //Método start dispara a execução da classe que herda a Thread.
+		new MovimentoAutomatico().start();  //MÃ©todo start dispara a execuÃ§Ã£o da classe que herda a Thread.
 	}
 	
-	//Classe MovimentoAutomatico herda da Thread a capacidade de ser rodada em tempo de execução.
+	//Classe MovimentoAutomatico herda da Thread a capacidade de ser rodada em tempo de execuÃ§Ã£o.
 	public class MovimentoAutomatico extends Thread {
-		//Método run executa as atividades da Thread.
+		//MÃ©todo run executa as atividades da Thread.
 		public void run() {
-			while(true) {  //Enquanto for verdade, o programa vai rodar. Obs.: Neste caso é sempre, ou seja, enquanto a janela estiver aberta.
+			while(true) {  //Enquanto for verdade, o programa vai rodar. Obs.: Neste caso Ã© sempre, ou seja, enquanto a janela estiver aberta.
 				try {
-					sleep(20);  //Coloca a Thread para dormir em 20 milissegundos, para não passar a imagem tão rápido na tela.
+					sleep(20);  //Coloca a Thread para dormir em 20 milissegundos, para nÃ£o passar a imagem tÃ£o rÃ¡pido na tela.
 				}
 				catch(Exception e) {
 					
 				}
 				
 				//Os Ifs abaixo, verificam se a imagem atingiu as bordas da janela.
-				//Se a imagem atingir as bordas da janela, sua posição atual recebe adiciona uma acréscimo com o sinal oposto ao atual.
+				//Se a imagem atingir as bordas da janela, sua posiÃ§Ã£o atual recebe adiciona uma acrÃ©scimo com o sinal oposto ao atual.
 				//Ou seja, se estiver aumentando, decresce, e se estiver dimunuindo, acresce.
 				if(janela.getLabelImagem().getX() > 665 || janela.getLabelImagem().getX() < 0) {
 					velocidadeX = velocidadeX * (-1);
@@ -45,7 +39,7 @@ public class ControladorJanelaDVD {
 					velocidadeY = velocidadeY * (-1);
 				}
 				
-				//A posição da imagem é atualizada.
+				//A posiÃ§Ã£o da imagem Ã© atualizada.
 				janela.getLabelImagem().setLocation(janela.getLabelImagem().getX()+velocidadeX, janela.getLabelImagem().getY()+velocidadeY);  
 			}
 		}
