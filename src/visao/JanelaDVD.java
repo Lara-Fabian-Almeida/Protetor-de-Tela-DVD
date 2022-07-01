75a8974ba8c31fe2b7c6a8800ec685c320e52e1e
Lara Fábian <87339100+Lara-Fabian-Almeida@users.noreply.github.com>
2022-06-30 23:24:25 -0300

    Update JanelaDVD.java

--- src/visao/JanelaDVD.java
+++ src/visao/JanelaDVD.java
@@ -1,11 +1,5 @@
 /*
- * Universidade Estadual do Sudoeste da Bahia - UESB
- * Curso: Sistemas de InformaÁ„o     Semestre: 3∫       PerÌodo Letivo: 2020.1
- * Disciplina: LP II      Professor: Murilo Santana
- * Aluno(a): Lara F·bian Almeida Santos
- * Data: 31/05/2021
- *
- * Criar interface gr·fica de um protetor de tela de DVD, usando Thread e sleep.
+ * Criar interface gr√°fica de um protetor de tela de DVD, usando Thread e sleep.
  */
 
 package visao;
@@ -16,7 +10,7 @@ import javax.swing.JLabel;
 
 public class JanelaDVD extends JFrame {
 
-	//DeclaraÁ„o de vari·veis.
+	//Declara√ß√£o de vari√°veis.
 	private static final long serialVersionUID = 1L;
 	private JLabel labelImagemDeFundo, labelImagem;
     
@@ -24,36 +18,36 @@ public class JanelaDVD extends JFrame {
     public JanelaDVD() {
         //Janela.
     	super();  // Chama o construtor da classe pai (JFrame, no caso). 
-        setTitle("Protetor de Tela de DVD");  //DefiniÁ„o do tÌtulo da janela.
+        setTitle("Protetor de Tela de DVD");  //Defini√ß√£o do t√≠tulo da janela.
         setVisible(true);  //Configura a visibilidade da tela.
         setSize(800, 566); //Configura o tamanho da tela.
         setLocationRelativeTo(null); //Centraliza imagem na tela.
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //MÈtodo que fecha a janela.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //M√©todo que fecha a janela.
         setLayout(null);
-        setResizable(false); //Impede a maximizaÁ„o da tela.
-        getContentPane().add(getLabelImagem());  //Adiciona a imagem que se movimentar· na janela.
+        setResizable(false); //Impede a maximiza√ß√£o da tela.
+        getContentPane().add(getLabelImagem());  //Adiciona a imagem que se movimentar√° na janela.
         getContentPane().add(getLabelImagemDeFundo());  //Adiciona a imagem de fundo na janela.
 		repaint();
 		validate();
     }
     
-    //JLabel que contÈm a imagem de fundo.
+    //JLabel que cont√©m a imagem de fundo.
     public JLabel getLabelImagemDeFundo() {
     	
 		if(labelImagemDeFundo == null) {
 			labelImagemDeFundo = new JLabel(); //Instanciando o JLabel.
-			labelImagemDeFundo.setIcon(new ImageIcon(getClass().getResource("CÈu.png"))); //Define o Ìcone imagem que o JLabel exibir·.
+			labelImagemDeFundo.setIcon(new ImageIcon(getClass().getResource("C√©u.png"))); //Define o √≠cone imagem que o JLabel exibir√°.
 			labelImagemDeFundo.setBounds(0, 0, 800, 566); //x, y, largura, altura da imagem.
 		}
     	return labelImagemDeFundo;
     }
     
-    // JLabel que contÈm a imagem que se movimenta.
+    // JLabel que cont√©m a imagem que se movimenta.
     public JLabel getLabelImagem() {
     	
 		if(labelImagem == null) {
 			labelImagem = new JLabel(); //Instanciando o JLabel.
-			labelImagem.setIcon(new ImageIcon(getClass().getResource("Estrela.png")));  //Define o Ìcone imagem que o JLabel exibir·.
+			labelImagem.setIcon(new ImageIcon(getClass().getResource("Estrela.png")));  //Define o √≠cone imagem que o JLabel exibir√°.
             labelImagem.setBounds(350, 150, 200, 200); //x, y, largura, altura da imagem.
 		}
     	return labelImagem;
